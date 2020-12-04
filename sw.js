1b21225a87106530efe6714e95487b021b9ec3ee
Michael Briones <mbrionescueva@gmail.com>
2020-12-04 12:45:51 -0500

    Modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Imports
 importScripts('js/sw-utils.js') // éste archivo debe incluirse en el APP_SHELL
 
-const STATIC_CACHE    = 'statc-v2'
-const DYNAMIC_CACHE   = 'dynamic-v1'
+const STATIC_CACHE    = 'statc-v3'
+const DYNAMIC_CACHE   = 'dynamic-v2'
 const INMUTABLE_CACHE = 'inmutable-v1'
 
 const APP_SHELL = [
@@ -46,6 +46,10 @@ self.addEventListener('activate', e => {
         // Por cada caché (key) que encontramos (inmutable, dynamic, static), lo eliminamos
         keys.forEach( key => {
             if(key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key) // borra todos los caché menos el static
+            }
+
+            if(key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                 return caches.delete(key) // borra todos los caché menos el static
             }
         })
